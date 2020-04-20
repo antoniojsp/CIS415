6ddb0559342f3e4026acf470e35bc92ecccadcc0
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-04-19 21:25:04 -0700

    ya

--- Shared/Project1/command.c
+++ Shared/Project1/command.c
@@ -14,12 +14,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>//check if file is there
 //changeDir
-
 #include <errno.h>
-
+//copyFile
+#include <fcntl.h>//for open
+#include <sys/stat.h>//check if file is there
 
 struct stat st = {0};
-//No leaks!
+
 void listDir()
 {
     struct dirent *structure;
@@ -42,22 +43,19 @@ void showCurrentDir()
    free(path);
 }
 
-void makeDir(char* nombre){
+void makeDir(char *dirName){
 
     char* pathway;// "/yarayara/yireyire/hola"
     char* folder;// "./hola"
 
-    pathway = sunir(2, nombre);//sunir is a personalized string concant for paths.
-    //char* ruta = pathway;
-    folder = sunir(1, nombre);
-    //char* folder = carpeta;
+    pathway = sunir(2, dirName);//sunir is a personalized string concant for paths.
+    folder = sunir(1, dirName);
 
     if (stat(pathway, &st) == -1) {
         mkdir(folder, 0700);
     }
     else{
       printf("%s\n","Folder already exists. Choose another name." );//not for debugging but part of the program.
-      free(nombre);
       free(pathway);
       free(folder);
       exit(0);//maybe change
@@ -69,14 +67,137 @@ void makeDir(char* nombre){
 }
 
 void changeDir(char *dirName){
-    char *move = char_string(MAX_PATH);
-    strcpy(move, "nuevo" );
-    if(chdir(move) == 0){//change for variable http://man7.org/linux/man-pages/man2/chdir.2.html
+    //char *move = char_string(MAX_PATH);
+    //strcpy(move, "nuevo" );
+    if(chdir(dirName) == 0){//change for variable http://man7.org/linux/man-pages/man2/chdir.2.html
       //chdir return 0 if ok or 1 if any error is found. errno
-      printf("Moved to %s\n", getcwd(move, MAX_PATH));//confirmation for moving. NO DEBBUGING.
+      printf("Moved to %s\n", getcwd(dirName, MAX_PATH));//confirmation for moving. NO DEBBUGING.
     }else{
-      printf("Unable to access %s: %s\n",move, strerror(errno));//report error
+      printf("Unable to access %s: %s\n",dirName, strerror(errno));//report error
     }
-    free(move);
-    return 0;
+}
+
+void copyFile(char *sourcePath, char *destinationPath){
+/*
+  char *sourcePath = char_string(MAX_PATH);
+  char *salida = char_string(MAX_PATH);
+
+  strcpy(sourcePath, "hola11.txt");
+  strcpy(salida, "./salida.txt");*/
+
+  int read_code = -1;
+  int override = 0;//ask if override the file
+  int end_loop = 0;
+  size_t lenght = 3;
+
+  char* answer = char_string(MAX_PATH);
+  char* line = char_string(MAX_PATH);
+  while(end_loop == 0){//checks if the file exists or no
+
+      if (stat(sourcePath, &st) == -1){//checking for the file to copy if exists
+          printf("File not found.\n");
+          break;
+      }
+      else if(stat(destinationPath, &st) == -1){
+          // needs to check that exists.
+          int input = open(sourcePath, O_RDONLY);//source
+          if (input < 0){//checker
+              printf("Error opening '%s': %s\n",sourcePath, strerror(errno));//report error
+              break;
+          }
+
+          int output = open(destinationPath, O_CREAT | O_WRONLY, 0644);//destination
+          if (output < 0){//checker
+            break;
+          }
+
+          while(read_code!=0){//if 0, then end of the file.
+            read_code = read(input, line, MAX_PATH);
+            line[read_code] = '\0';//to signal the end.
+            write(output, line, strlen(line));//<--------------system call to write files.
+          }
+          override = 1;
+          close(input);
+          close(output);
+          if (override == 1){
+              break;
+          }
+      }else{
+          printf("%s", "Override?(Y/N): ");//checks if it is overwritten
+          getline(&answer,&lenght,stdin);
+          if(strcmp(answer, "Y\n") == 0 || strcmp(answer, "y\n") == 0){
+              unlink(destinationPath);//delete file first
+          }else{
+              break;
+          }
+      }
+    }
+
+    free(answer);
+    free(line);
+
+}
+
+void moveFile(char *sourcePath, char *destinationPath){
+
+  int read_code = -1;
+  int override = 0;//ask if override the file
+  int end_loop = 0;
+  size_t lenght = 3;
+
+  char* answer = (char*)calloc(lenght,sizeof(char));
+  //char* answer = respuesta;
+  char* line =(char*)calloc(MAX_PATH,sizeof(char));
+  //char* line = linea;
+  while(end_loop == 0){//checks if the file exists or no
+      if(stat(sourcePath, &st) == -1){
+          printf("%s\n",  "File not found." );
+          break;
+          }
+      else if (stat(destinationPath, &st) == -1){
+          int input = open(sourcePath, O_RDONLY);//source
+          if (input < 0){//checker
+              exit(1);
+          }
+
+          int output = open(destinationPath, O_CREAT | O_WRONLY, 0644);//destination
+          if (output < 0){//checker
+            exit(1);
+          }
+
+          while(read_code!=0){//if 0, then end of the file.
+            read_code = read(input, line, MAX_PATH);
+            line[read_code] = '\0';//to signal the end.
+            write(output, line, strlen(line));//<--------------system call to write files.
+          }
+          unlink(sourcePath);//delete file first
+          override = 1;
+          close(input);
+          close(output);
+          if (override == 1){
+              break;
+          }
+      }else{
+          printf("%s", "Override?(Y/N): ");//checks if it is overwritten
+          getline(&answer,&lenght,stdin);
+          if(strcmp(answer, "Y\n") == 0 || strcmp(answer, "y\n") == 0){
+            unlink(destinationPath);//delete file first
+          }else{
+              break;
+          }
+      }
+    }
+
+    free(answer);
+    free(line);
+}
+
+void deleteFile(char *filename){
+    //  "/media/sf_CIS415/hola1.txt"-> how to enter for differnent path than current.
+    //  hola.txt   -> current directory
+    if(unlink(filename) == 0){
+
+    }else{
+        printf("Error with '%s': %s\n",filename, strerror(errno));//Show errors of why the file is not deleted. If it doesn't exists it will say it.
+    }
 }
--- Shared/Project1/main.c
+++ Shared/Project1/main.c
@@ -2,13 +2,39 @@
 #include "command.h"
 #include "addons.h"
 #include <string.h>
+#include <stdlib.h>//for callo
 #define MAX_PATH 260
 int main(){
 
   //listDir();
   //showCurrentDir();
-  char* nombre = char_string(MAX_PATH);
-  strcpy(nombre,"ll");
-  makeDir(nombre);
+
+  //char* nombre = char_string(MAX_PATH);
+  //strcpy(nombre,"lla");
+  //makeDir(nombre);
+
+  //char* cambio = char_string(MAX_PATH);
+  //strcpy(cambio,"..");
+  //changeDir(cambio);
+
+  /*char* sourcePath = char_string(MAX_PATH);
+  char* destinationPath = char_string(MAX_PATH);
+  strcpy(sourcePath, "texto.txt");
+  strcpy(destinationPath, "./salida.txt");
+  copyFile(sourcePath, destinationPath);
+  free(sourcePath);
+  free(destinationPath);*/
+
+  /*char* sourcePath = char_string(MAX_PATH);
+  char* destinationPath = char_string(MAX_PATH);
+  strcpy(sourcePath, "salida.txt");
+  strcpy(destinationPath,"/media/sf_CIS415/hola1.txt");
+  moveFile(sourcePath,destinationPath); */
+  char* filename = char_string(MAX_PATH);
+  strcpy(filename, "texto.txt");
+  deleteFile(filename);
+  //free(sourcePath);
+  free(filename);
+
   return 0;
 }
