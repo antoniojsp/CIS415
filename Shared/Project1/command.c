83ac2f2f9a5f0d8be2e70deabbb6e8911ac49219
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-04-21 14:08:43 -0700

    interactivo

--- Shared/Project1/command.c
+++ Shared/Project1/command.c
@@ -13,7 +13,6 @@
 
 #define MAX_PATH 260//maximum number of char allowed for path
 
-
 struct stat st = {0};
 
 void listDir()
@@ -34,71 +33,55 @@ void showCurrentDir()
 {
    char *path = char_string(MAX_PATH);//path of the file
    getcwd(path, MAX_PATH);//system call for path
-   printf("Path: %s\n", path);
+   printf("%s\n", path);
    free(path);
 }
 
 void makeDir(char *dirName)
 {
     dirName[strlen(dirName)-1]=0;
-    //char* pathway = char_string(MAX_PATH);// "/yarayara/yireyire/hola"
-    char* folder = char_string(MAX_PATH);// "./hola"
-    //pathway = sunir(2, dirName);//sunir is a personalized string concant for paths.
-    //folder = sunir(1, dirName);
-    //strcpy(pathway,sunir(2, dirName));
+    char* folder = char_string(MAX_PATH);//hold the name of the folder to create a path for mkdir()
     char* temp = sunir(1, dirName);
     strcpy(folder,temp);
 
-    //if (stat(pathway, &st) == -1) {
     if(mkdir(folder, 0700)==0){
     }
     else{
       printf("Unable to access %s: %s\n",dirName, strerror(errno));//not for debugging but part of the program.
-
     }
-    //free(pathway);
     free(folder);
     free(temp);
 }
 
 void changeDir(char *dirName)//check aqui
 {
-  dirName[strlen(dirName)-1]=0;
-
+  dirName[strlen(dirName)-1]=0;//clean the last chracter
 
   int change =chdir(dirName);//change for variable http://man7.org/linux/man-pages/man2/chdir.2.html
     //chdir return 0 if ok or 1 if any error is found. errno
-  if (change==0){
-    printf("Moved to %s\n", dirName );
+  if (change!=0){//if sucessful, chdir() returns 0.
+    printf("%s\n",strerror(errno));
   }
-  else{
-    printf("Cannot moved. %s\n",strerror(errno));
-  }
-
 }
 
 void copyFile(char *sourcePath, char *destinationPath)
 {
-  //sourcePath[strlen(sourcePath)-1] = 0;
-  destinationPath[strlen(destinationPath)-1]= 0;
-
+  destinationPath[strlen(destinationPath)-1]= 0;//clean last character
   int read_code = -1;
   int override = 0;//ask if override the file
-  int end_loop = 0;
+  int end_loop = 0;//loop ending signal
   size_t lenght = 3;
 
   char* answer = char_string(MAX_PATH);
   char* line = char_string(MAX_PATH);
   while(end_loop == 0){//checks if the file exists or no
-
       if (stat(sourcePath, &st) == -1){//checking for the file to copy if exists
-          printf("File not found.\n");
           break;
       }
       else if(stat(destinationPath, &st) == -1){// needs to check that exists.
           int input = open(sourcePath, O_RDONLY);//source
           if (input < 0){//checker
-              printf("Error opening '%s': %s\n",sourcePath, strerror(errno));//report error
+              //printf("Error opening '%s': %s\n",sourcePath, strerror(errno));//report errors with the errno.h. Tells us what kind of problem happened.
               break;
           }
 
@@ -130,16 +113,13 @@ void copyFile(char *sourcePath, char *destinationPath)
     }
     free(answer);
     free(line);
-
 }
 
 void moveFile(char *sourcePath, char *destinationPath){
 
   sourcePath[strlen(sourcePath)]=0;//clean the end and set it to 0 to signal the end.
   destinationPath[strlen(destinationPath)-1]=0;
-  //strcpy(sourcePath, "ejemplo.txt");
-  //strcpy(destinationPath, "maria.txt");
-  printf("%s %s\n",sourcePath, destinationPath );
+
   int read_code = -1;
   int override = 0;//ask if override the file
   int end_loop = 0;
@@ -149,18 +129,17 @@ void moveFile(char *sourcePath, char *destinationPath){
   char* line =char_string(MAX_PATH);
   while(end_loop == 0){//checks if the file exists or no
       if(stat(sourcePath, &st) == -1){
-          printf("%s\n",  "Source file not found." );//checker to see if the file exists.
           break;
           }
       else if (stat(destinationPath, &st) == -1){
           int input = open(sourcePath, O_RDONLY);//source
           if (input < 0){//checker
-              exit(1);
+              break;
           }
 
           int output = open(destinationPath, O_CREAT | O_WRONLY, 0644);//destination
           if (output < 0){//checker
-            exit(1);
+            break;
           }
 
           while(read_code!=0){//if 0, then end of the file.
@@ -193,11 +172,11 @@ void deleteFile(char *filename){
     //  "/media/sf_CIS415/hola1.txt"-> how to enter for differnent path than current.
     //  hola.txt   -> current directory
     filename[strlen(filename)-1]=0;
-    if(unlink(filename) == 0){
-    }else{
-        printf("Error with '%s': %s\n",filename, strerror(errno));//Show errors of why the file is not deleted. If it doesn't exists it will say it.
+    if(unlink(filename) != 0){
+        printf("%s\n", strerror(errno));//Show errors of why the file is not deleted. If it doesn't exists it will say it.
     }
 }
+
 void displayFile(char *filename)
 {
     filename[strlen(filename)-1]=0;
@@ -205,11 +184,7 @@ void displayFile(char *filename)
     char* line = char_string(MAX_PATH);
     if (stat(filename, &st) != -1){
         // needs to check that exists.
-          int input = open(filename, O_RDONLY);//source
-          if (input < 0){//checker
-              printf("Error opening '%s': %s\n",filename, strerror(errno));
-              exit(1);
-          }
+          int input = open(filename, O_RDONLY);//source, it is already checked to exists from the stat fuction.
           while(read_code!=0){//if 0, then end of the file.
             read_code = read(input, line, MAX_PATH);
             line[read_code] = '\0';//to signal the end.
@@ -217,8 +192,7 @@ void displayFile(char *filename)
           }
           close(input);
     }else{
-        printf("%s","File doesn't exists.\n");//print statement that indicates the file is not found. It's not used for debugging but as an alert for the program itself.
+        printf("%s",strerror(errno));//print statement that indicates the file is not found. It's not used for debugging but as an alert for the program itself.
     }
     free(line);
-    //free(archivo);
 }
--- Shared/Project1/main.c
+++ Shared/Project1/main.c
@@ -9,7 +9,7 @@
 #define TOKEN_BUFFER 100
 #define LINES 500//for the file reader. File MODE. It can accept up to 500 lines of instructions from the file.
 
-int main(){
+int main(int argc, char* argv[]){
 
       int exit_loop = 0;
       char** list_commands = array_2d(COMMAND_LIST,BUFFER);
@@ -25,17 +25,18 @@ int main(){
 
       //FILE MODE
       char** file_mode = array_2d(LINES, BUFFER);
-      char* modo = char_string(10);//
-      strcpy(modo, "-f");
+      //char* modo = char_string(10);//
+      //strcpy(modo, "-f");
+      int lineas_file=0;
 
-      if(strcmp(modo,"-f")==0){
-        printf("%s\n", "aaa");
+      if(strcmp(argv[1],"-f")==0){
+
           FILE *file_reader = fopen("input.txt", "r");
           if (file_reader == NULL){// if no input.txt found
             exit(1);
           }
 
-          int lineas_file=0;
+
           char buffer[BUFFER];
           while(fgets(buffer,TOKEN_BUFFER, file_reader) != NULL){
                 strcpy(file_mode[lineas_file],buffer);
@@ -45,16 +46,16 @@ int main(){
       }
 
 
-
+      //printf("%d\n", lineas_file );
       int modo_archivo = 0;
-      while(exit_loop == 0){
+      while(exit_loop == 0 && modo_archivo < lineas_file){
           printf("%s",">>>");
-          if(strcmp(modo,"-f")!=0){
+          if(strcmp(argv[1],"-f")!=0){
             getline(&line_command, &size, stdin);
             strcpy(linea,line_command);
-          }
-          else{
+          }else{
             strcpy(linea,file_mode[modo_archivo]);
+            printf("%s\n",linea);
           }
 
           char*line = linea;
@@ -63,11 +64,8 @@ int main(){
             strcpy(list[number_commands],parte);// copy to an array of strings,  I used it this way for reuse the code in future projects.
             number_commands+=1;
           }
-          for (size_t i = 0; i < number_commands; i++) {
-            /* code */
-            printf("%s\n",list[i] );
-          }
-         for (int i = 0; i < number_commands; i++) {/*
+
+          for (int i = 0; i < number_commands; i++) {
                 char** token = array_2d(LIST_INPUT, BUFFER);
                 char* temp = char_string(BUFFER);
                 strcpy(temp,list[i]);
@@ -78,112 +76,102 @@ int main(){
                   strcpy(token[number_tokens],parte);// copy to an array of strings,  I used it this way for reuse the code in future projects.
                   number_tokens+=1;
                 }
-
                 int control = calls(token,number_tokens);//function  that checks control code.
 
                 if(control == 1){
-                if(strcmp(token[0],"exit\n") == 0 || strcmp(token[0],"exit\0") == 0){
-                    if(strcmp(token[1],"\0") == 0){
-                      exit_loop = 1;
+                    if(strcmp(token[0],"exit\n") == 0 || strcmp(token[0],"exit\0") == 0){
+                        if(strcmp(token[1],"\0") == 0){
+                          exit_loop = 1;
+                        }
+                        else{
+                          printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                        }
                     }
-                    else{
-                      printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                    else if((strcmp(token[0],"\n"))==0){//check for empty lines.
+                        printf("%s","");
                     }
-                }
-                else if((strcmp(line_command,"\n"))==0){//check for empty lines.
-                    printf("%s","");
-                }
-                else if(strcmp(token[0],"ls\n") == 0 || strcmp(token[0],"ls\0") == 0 ){//check for empty lines.
-                  if(strcmp(token[1],"\0") == 0)
-                    listDir();
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
-                }
-                else if(strcmp(token[0],"pwd\n") == 0 || strcmp(token[0],"pwd\0") == 0 ){//check for empty lines.
-                  if(strcmp(token[1],"\0") == 0)
-                  {
-                    showCurrentDir();
-                  }
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
-                }
-
-                else if(strcmp(token[0],"mkdir\n") == 0 || strcmp(token[0],"mkdir\0") == 0 ){//check for empty lines.
-
-
-                  if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0)
-
-                    makeDir(token[1]);
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",list[0]);
-                  }
-                }
-
-                else if(strcmp(token[0],"cd\n") == 0 || strcmp(token[0],"cd\0") == 0){//check for empty lines.
-
-                    if(strcmp(token[1],"\n") != 0 && strcmp(token[2],"\0") == 0 && strcmp(token[2],"\0") == 0){
-                      changeDir(token[1]);
+                    else if(strcmp(token[0],"ls\n") == 0 || strcmp(token[0],"ls\0") == 0 ){//check for empty lines.
+                      if(strcmp(token[1],"\0") == 0)
+                        listDir();
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
                     }
-                    else{
-                      printf("Error!: Unsupported parameter for command: %s\n",list[0]);
+                    else if(strcmp(token[0],"pwd\n") == 0 || strcmp(token[0],"pwd\0") == 0 ){//check for empty lines.
+                      if(strcmp(token[1],"\0") == 0)
+                      {
+                        showCurrentDir();
+                      }
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
                     }
-                }
-                else if(strcmp(token[0],"cp\n") == 0 || strcmp(token[0],"cp\0")==0){//check for empty lines.
+                    else if(strcmp(token[0],"mkdir\n") == 0 || strcmp(token[0],"mkdir\0") == 0 ){//check for empty lines.
+                      if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0)
 
-                  if(strcmp(token[2],"\0") != 0 && strcmp(token[3],"\0") == 0){
-                    copyFile(token[1], token[2]);
-                  }
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
-                }
+                        makeDir(token[1]);
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",list[0]);
+                      }
+                    }
 
-                else if(strcmp(token[0],"mv\n") == 0 || strcmp(token[0],"mv\0") == 0){//check for empty lines.
-                  if( strcmp(token[2],"\0") != 0 && strcmp(token[3],"\0") == 0){
-                    moveFile(token[1], token[2]);
-                  }
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
+                    else if(strcmp(token[0],"cd\n") == 0 || strcmp(token[0],"cd\0") == 0){//check for empty lines.
+                        if(strcmp(token[1],"\n") != 0 && strcmp(token[2],"\0") == 0 && strcmp(token[2],"\0") == 0){
+                          changeDir(token[1]);
+                        }
+                        else{
+                          printf("Error!: Unsupported parameter for command: %s\n",list[0]);
+                        }
+                    }
 
-                }
-                else if(strcmp(token[0],"rm\n") == 0 || strcmp(token[0],"rm\0") == 0){//check for empty lines.
+                    else if(strcmp(token[0],"cp\n") == 0 || strcmp(token[0],"cp\0")==0){//check for empty lines.
+                      if(strcmp(token[2],"\0") != 0 && strcmp(token[3],"\0") == 0){
+                        copyFile(token[1], token[2]);
+                      }
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
+                    }
 
-                  if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0)
-                    deleteFile(token[1]);
-                  else{
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
-                }
+                    else if(strcmp(token[0],"mv\n") == 0 || strcmp(token[0],"mv\0") == 0){//check for empty lines.
+                      if( strcmp(token[2],"\0") != 0 && strcmp(token[3],"\0") == 0){
+                        moveFile(token[1], token[2]);
+                      }
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
+                    }
 
-                else if(strcmp(token[0],"cat\n") == 0 || strcmp(token[0],"cat\0") == 0){//check for empty lines.
+                    else if(strcmp(token[0],"rm\n") == 0 || strcmp(token[0],"rm\0") == 0){//check for empty lines.
 
-                  if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0){
-                      displayFile(token[1]);
-                  }
-                  else {
-                    printf("Error!: Unsupported parameter for command: %s\n",token[0]);
-                  }
-                }
+                      if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0)
+                        deleteFile(token[1]);
+                      else{
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
+                    }
 
-                else{//check for empty lines.
+                    else if(strcmp(token[0],"cat\n") == 0 || strcmp(token[0],"cat\0") == 0){//check for empty lines.
 
-                    printf("Error! Unrecognized command: %s\n", token[0]);
-                }
+                      if(strcmp(token[1],"\0") != 0 && strcmp(token[2],"\0") == 0){
+                          displayFile(token[1]);
+                      }
+                      else {
+                        printf("Error!: Unsupported parameter for command: %s\n",token[0]);
+                      }
+                    }
 
-
+                    else{//check for empty lines.
+                        printf("Error! Unrecognized command: %s\n", token[0]);
+                    }
               }
               else{
-                printf("Error! Incorrect syntax. No control codefound,\n");
+                  printf("Error! Incorrect syntax. No control codefound,\n");
               }
               free_double(token, LIST_INPUT);
               free(temp);
-*/
-
         }
-          modo_archivo++;
+        modo_archivo++;
     }
 
     free_double(list_commands, COMMAND_LIST);
